c50df552731fb8aa0ad9003ff8c72a195b5fc703
VoiceOfSilense <erik-grigoryan-1996@inbox.ru>
2016-10-24 15:20:22 +0400

    k

--- js/script.js
+++ js/script.js
@@ -19,6 +19,6 @@ $(function(){
 $(function (e) {
     $('.arrow_to_start').click(function () {
         $('.zanaves').hide();
-        $('.main_page').fadeIn();
+        $('.main_page').fadeIn('slow');
     })
 })
