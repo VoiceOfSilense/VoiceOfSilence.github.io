ce7f11b68022815505491fc7ebcecde23694a9bf
VoiceOfSilense <erik-grigoryan-1996@inbox.ru>
2016-10-24 15:15:57 +0400

    f

--- js/script.js
+++ js/script.js
@@ -19,6 +19,6 @@ $(function(){
 $(function (e) {
     $('.arrow_to_start').click(function () {
         $('.zanaves').hide();
-        $('.main_page').slideToggle();
+        $('.main_page').show();
     })
 })
